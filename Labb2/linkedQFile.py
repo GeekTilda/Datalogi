7d1c199794834992f2d67986fbdd4c37d039ddd5
Tilda L Aderud <tilda6633@gmail.com>
2024-08-29 22:28:21 +0200

    Stage 6 complete

---
+++ Labb2/linkedQFile.py
@@ -0,0 +1,51 @@
+class Node():
+
+    def __init__(self, value = None):
+        self.value = value
+        self.next = None
+
+    def setNext(self,new):
+        self.next = new
+
+    def getNext(self):
+        return self.next
+
+    def setValue(self,new):
+        self.value = new
+
+    def getValue(self):
+        return self.value
+
+    def nextNode(self):
+        return self.next
+
+class LinkedQ():
+
+    def __init__(self):
+        self._first = None
+        self._last = None
+
+    def enqueue(self, value):    # Adds a new node to the end and makes sure that the list isnt empty
+        newNode = Node(value)
+        if not self.isEmpty():
+            self._last.setNext(newNode)
+            self._last = newNode
+        else: 
+            self._first = newNode
+            self._last = newNode
+
+    def dequeue(self):  # Makes the new first node the next node, and returns the value (aka removes the first node)
+        if self.isEmpty():
+            return None
+        temp = self._first
+        self._first = self._first.getNext()
+        if self._first is None:
+            self._last = None
+        return temp.getValue()
+    
+    def isEmpty(self):  # Checks if the list is empty
+        if self._first == None:
+            return True
+        else:
+            return False
+        
--- Labb2/main.py
+++ Labb2/main.py
@@ -21,3 +21,30 @@ def CardTrick(queue):
 ### CALLING MAIN
 
 main()
+
+### TESTING
+
+import unittest
+from linkedQFile import LinkedQ
+
+class TestQueue(unittest.TestCase):
+
+    def test_isEmpty(self):
+        #isEmpty ska returnera True för tom kö, False annars
+        q = LinkedQ()
+        self.assertTrue(q.isEmpty(), "isEmpty på tom kö")
+        q.enqueue(17)
+        self.assertFalse(q.isEmpty(), "isEmpty på icke-tom kö")
+
+    def test_order(self):
+        #Kontrollerar att kö-ordningen blir rätt
+        q = LinkedQ()
+        q.enqueue(1)
+        q.enqueue(2)
+        q.enqueue(3)
+        self.assertEqual(q.dequeue(), 1)
+        self.assertEqual(q.dequeue(), 2)
+        self.assertEqual(q.dequeue(), 3)
+
+if __name__ == "__main__":
+    unittest.main()
