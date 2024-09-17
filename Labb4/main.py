559f80ec11ef16d57df9f1ef91c7593b2a790fb6
melker-e <127298226+melker-e@users.noreply.github.com>
2024-09-17 14:04:23 +0200

    Update main.py
    formatterade om lite

--- Labb4/main.py
+++ Labb4/main.py
@@ -1,37 +1,37 @@
 from bintreeFile import Bintree
 from linkedQFile import LinkedQ
 
-svenska = Bintree()
-gamla = Bintree()
-q = LinkedQ()
-
-with open("word3.txt", "r", encoding = "utf-8") as svenskfil:
-    for rad in svenskfil:
-        ordet = rad.strip()                # Ett trebokstavsord per rad
-        svenska.put(ordet)                 # in i sökträdet
-
-def makeChildren(startWord, queue):
-    letters = "abcdefghijklmnopqrstuvwxyzåäö"
-    gamla.put(startWord)
-
-    for i in range(len(startWord)):
-        for letter in letters:
-            newWord = startWord.replace(startWord[i], letter)
-
-            if (newWord in svenska) and (newWord not in gamla):
-                queue.enqueue(newWord)
-                gamla.put(newWord)
-
 def main():
     startWord = input("Ge ett startord: ")
     endWord = input("Ge ett slutord: ")
 
+    svenska = Bintree()     # Bintree with all words.
+    gamla = Bintree()       # Bintree with used words.
+    q = LinkedQ()           # Queue for going through words for wordsearch.
+
+    with open("word3.txt", "r", encoding = "utf-8") as svenskfil:
+        for rad in svenskfil:
+            ordet = rad.strip()                # One three-letter-word per row.
+            svenska.put(ordet)                 # Puts it into the Bintree.
+
     q.enqueue(startWord)
-    while not q.isEmpty():
+    while not q.isEmpty():  # Searches through all words related to startWord until it finds endWord.
         word = q.dequeue()
         if word == endWord:
             print(f"Det finns en väg till {endWord}!"); quit()
-        makeChildren(word, q)
+        makeChildren(word, q, svenska, gamla)
     print(f"Det finns ingen väg till {endWord}...")
+
+def makeChildren(startWord, queue, svenska, gamla):
+    letters = "abcdefghijklmnopqrstuvwxyzåäö"       # Characters.
+    gamla.put(startWord)
+
+    for i in range(len(startWord)):
+        for letter in letters:
+            newWord = startWord.replace(startWord[i], letter)       # Creates a new word with one letter changed.
+
+            if (newWord in svenska) and (newWord not in gamla):     # Adds it to the word-queue if newWord is valid.
+                queue.enqueue(newWord)
+                gamla.put(newWord)
 
 main()
